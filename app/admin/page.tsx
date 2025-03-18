bb07993d15ee2e761457fe45e44cb06bcf36e19e
ValentinChanter <valentinsing19@gmail.com>
2025-03-18 02:45:52 +0100

    Edit css for lighter colors

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -101,14 +101,14 @@ export default function Admin() {
                     {currentLevel === 0 && (
                         <form className="flex flex-col self-center" onSubmit={(e) => start(e)}>
                             <label htmlFor="username" className="mb-2">Enter your username:</label>
-                            <input id="username" type="text" className="bg-slate-800 rounded-lg px-2 py-1" pattern="\w{2,}" required onChange={(e) => setUsername(e.target.value)} value={username} title="Please use at least 2 alphanumeric characters (\w{2,})"></input>
+                            <input id="username" type="text" className="bg-gray-700 rounded-lg px-2 py-1" pattern="\w{2,}" required onChange={(e) => setUsername(e.target.value)} value={username} title="Please use at least 2 alphanumeric characters (\w{2,})"></input>
                         </form>
                     )}
 
                     {currentLevel === 1 && (
                         <form className="flex flex-col self-center" onSubmit={(e) => checkLevel1(e)}>
                             <div className="mb-2 flex flex-col">
-                                <span>You have been given a JWT to track your progress.</span>
+                                <span>You have been given a token to track your progress.</span>
                                 <span className="flex flex-row justify-center">Please do not lose or modify it.</span>
                             </div>
                             <button disabled className="rounded-full border-slate-500 border-1 px-4 py-3 cursor-not-allowed">Got it. Take me to Level 2.</button>
@@ -118,7 +118,7 @@ export default function Admin() {
                     {currentLevel === 2 && (
                         <form className="flex flex-col self-center" onSubmit={(e) => checkLevel2(e)}>
                             <label htmlFor="password" className="mb-2">Enter password:</label>
-                            <input id="password" type="password" disabled className="bg-slate-800 rounded-lg px-2 py-1 cursor-not-allowed" onChange={(e) => setLevel2Password(e.target.value)} value={level2Password}></input>
+                            <input id="password" type="password" disabled className="bg-gray-700 rounded-lg px-2 py-1 cursor-not-allowed" onChange={(e) => setLevel2Password(e.target.value)} value={level2Password}></input>
                         </form>
                     )}
 
@@ -128,7 +128,7 @@ export default function Admin() {
 
                     {currentLevel > 1 ? (
                         <div className="flex flex-row justify-end">
-                            <button className="rounded-full border-red-600 border-1 px-4 py-3 cursor-pointer hover:bg-red-600" onClick={() => end()}>Forfeit</button>
+                            <button className="rounded-full border-red-500 border-1 px-4 py-3 cursor-pointer hover:bg-red-500" onClick={() => end()}>Forfeit</button>
                         </div>
                     ) : (
                         <div></div>
