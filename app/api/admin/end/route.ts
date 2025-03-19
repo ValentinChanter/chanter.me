25d97bbe1774af21f05076927522bb8ed95bde03
ValentinChanter <valentinsing19@gmail.com>
2025-03-19 03:41:27 +0100

    Remove unused declaration

--- app/api/admin/end/route.ts
+++ app/api/admin/end/route.ts
@@ -1,15 +1,6 @@
 import { NextRequest } from "next/server";
 import * as jwt from "jsonwebtoken";
 
-declare module "jsonwebtoken" {
-    export interface JwtPayload {
-        username: string;
-        level1: number;
-        level2?: number;
-        // TODO: Add the rest
-    }
-}
-
 export async function POST(req: NextRequest) {
     const body = await req.json();
     const { token } = body;
