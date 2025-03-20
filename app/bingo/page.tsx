fcfb6c44e525bc1720fcc45ea996f799a8fcf239
ValentinChanter <valentinsing19@gmail.com>
2025-03-20 02:59:14 +0100

    Change logic from login to code-based rooms

--- app/bingo/page.tsx
+++ app/bingo/page.tsx
@@ -1,160 +1,104 @@
 "use client";
 
 import { FormEvent, useEffect, useState } from "react";
-import { useSetCookie, useHasCookie, useDeleteCookie, useGetCookie } from 'cookies-next/client';
-import { useRouter } from 'next/navigation'
 import { jwtDecode } from "jwt-decode";
-import { useCells } from "../api/bingo/socket/websocket";
-import { Cell } from "../api/bingo/socket/bingo";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useSetCookie } from 'cookies-next/client';
 
-export default function Bingo() {
-    const router = useRouter()
-
+export default function BingoHome() {
     const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loggedAs, setLoggedAs] = useState("");
+    const [code, setCode] = useState("");
+    const [token, setToken] = useState("");
+    const [createError, setCreateError] = useState("");
+    const [loading, setLoading] = useState(false);
 
+    const router = useRouter();
+    const searchParams = useSearchParams();
     const setCookie = useSetCookie();
-    const getCookie = useGetCookie();
-    const hasCookie = useHasCookie();
-    const deleteCookie = useDeleteCookie();
 
-    function login(e: FormEvent) {
-        fetch("/api/bingo/login", {
+    function createRoom(e: FormEvent) {
+        setLoading(true);
+        fetch("/api/bingo/createRoom", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify({ username, token }),
         }).then((res) => {
             if (res.ok) {
                 return res.json();
+            } else {
+                switch (res.status) {
+                    case 400:
+                        setCreateError("Paramètres manquants. Veuillez réessayer.");
+                        break;
+
+                    case 401:
+                        setCreateError("Jeton invalide. Veuillez réessayer.");
+                        break;
+
+                    default:
+                        setCreateError("Une erreur inconnue s'est produite. Veuillez réessayer.");
+                        break;
+                }
             }
         }).then((data) => {
             if (data) {
+                const decoded = jwtDecode<{ username: string, id: string, code: string }>(data.token);
                 setCookie('jwt', data.token);
-                const decodedToken = jwtDecode<{ username: string }>(data.token);
-                setLoggedAs(decodedToken.username);
+
+                router.push(`/bingo/${decoded.code}`);
             }
         });
 
         e.preventDefault();
     }
 
-    function generateGrid() {
-        fetch("/api/bingo/generate", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ token: getCookie("jwt") }),
-        }).then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-        }).then((data) => {
-            if (data) {
-                console.log(data);
-
-                // Set new JWT
-                setCookie('jwt', data.token);
-
-                // Set grid using method defined in WebSocket
-                const grid = data.words.map((word: string) => ({ word, colors: [] })) as Cell[];
-                sendGrid(grid);
-            }
-        });
+    function joinRoom(e: FormEvent) {
+        e.preventDefault();
     }
 
-    // WebSocket setup
-    const [cells, sendGrid, setGrid] = useCells(() => `ws${process.env.NODE_ENV === "production" ? "s" : ""}://${window.location.host}/api/bingo/socket`);
-
     useEffect(() => {
-        if (hasCookie("jwt")) {
-            const token = getCookie("jwt") as string;
-            try {
-                const decodedToken = jwtDecode<{ username: string }>(token);
-                setLoggedAs(decodedToken.username);
-
-                // Fetch grid
-                fetch("/api/bingo/getGrid", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({ token }),
-                }).then((res) => {
-                    if (res.ok) {
-                        return res.json();
-                    }
-                }
-                ).then((data) => {
-                    if (data) {
-                        // Send grid to WebSocket
-                        const grid = data.grid as Cell[];
-                        setGrid(grid);
-                    }
-                });
-            } catch {
-                deleteCookie("jwt");
-                setLoggedAs("");
-                router.refresh();
-            }
+        if (searchParams.has("code")) {
+            setCode(searchParams.get("code") as string);
         }
-    }, [hasCookie, getCookie, setLoggedAs, deleteCookie, router, setGrid]);
+    }, [searchParams]);
 
     return (
         <div className='flex h-screen'>
-            {!hasCookie("jwt") && !loggedAs ? (
-                <form className="flex flex-col justify-between m-auto rounded-xl px-12 py-8 w-1/3 h-[350px] dark-rectangle" onSubmit={(e) => login(e)}>
-                    <div>
-                        <div className="flex flex-col space-y-2 mb-8">
-                            <label htmlFor="username">Nom d&apos;utilisateur</label>
-                            <input id="username" type="text" value={username} className="bg-gray-700 rounded-lg px-4 py-2 outline outline-white/10" onChange={(e) => setUsername(e.target.value)}></input>
-                        </div>
-                        <div className="flex flex-col space-y-2">
-                            <label htmlFor="password">Mot de passe</label>
-                            <input id="password" type="password" value={password} className="bg-gray-700 rounded-lg px-4 py-2 outline outline-white/10" onChange={(e) => setPassword(e.target.value)}></input>
-                        </div>
+            <form className="flex flex-col justify-between m-auto rounded-xl px-12 py-8 w-1/3 h-[450px] dark-rectangle" onSubmit={(e) => joinRoom(e)}>
+                <div>
+                    <span className="text-3xl">Rejoindre un salon</span>
+                    <div className="flex flex-col space-y-2 my-8">
+                        <label htmlFor="joinUsername">Nom d&apos;utilisateur</label>
+                        <input required id="joinUsername" type="text" value={username} className="bg-gray-700 rounded-lg px-4 py-2 outline outline-white/10" onChange={(e) => setUsername(e.target.value)}></input>
                     </div>
-                    <button className="py-4 mt-8 w-full cursor-pointer dark-button">Se connecter</button>
-                </form>
-            ) : (
-                <div className="flex flex-row m-auto justify-between w-full h-full px-20 py-8">
-                    <div className="rounded-xl p-4 h-5/6 aspect-square my-auto dark-rectangle ">
-                        <div className="grid grid-cols-5 grid-rows-5 gap-2">
-                            {cells.length === 25 ? (
-                                cells.map((cell, i) => (
-                                    <div key={i} className="outline outline-white/15 rounded-lg p-1 text-white overflow-y-auto no-scrollbar aspect-square text-center cursor-pointer hover:bg-gray-600 active:bg-gray-700 text-sm flex">
-                                        <div className="overflow-x-auto m-auto">
-                                            <span className="break-words">{cell.word.replace(/_/g, " ") || i + 1}</span>
-                                        </div>
-                                    </div>
-                                )
-                            )) : (
-                                Array.from({ length: 25 }, (_, i) => (
-                                    <div key={i} className="outline outline-white/15 rounded-lg p-4 text-white aspect-square text-center flex flex-col justify-center cursor-pointer hover:bg-gray-600 active:bg-gray-700"></div>
-                                ))
-                            )}
-                        </div>
-                    </div>
-                    <div className="rounded-xl p-6 flex flex-col justify-between w-1/5 h-5/6 my-auto dark-rectangle">
-                        <div>
-                            <div className="flex flex-col mb-8">
-                                <span className="text-xl">Joueurs en ligne :</span>
-                                <span>test</span>
-                            </div>
-                            <div className="flex flex-col">
-                                <span className="text-xl">Mot de départ :</span>
-                                <span>Non défini</span>
-                            </div>
-                        </div>
-                        <div>
-                            <button disabled={loggedAs !== "Admin"} className="dark-button py-4 w-full cursor-pointer disabled:cursor-not-allowed" onClick={() => generateGrid()}>Générer une nouvelle grille</button>
-                        </div>
+                    <div className="flex flex-col space-y-2">
+                        <label htmlFor="code">Code du salon</label>
+                        <input required id="code" type="text" value={code} className="bg-gray-700 rounded-lg px-4 py-2 outline outline-white/10" onChange={(e) => setCode(e.target.value)}></input>
                     </div>
                 </div>
-            )}
+                <button className="py-4 mt-8 w-full cursor-pointer dark-button">Rejoindre le salon</button>
+            </form>
+
+            <form className="flex flex-col justify-between m-auto rounded-xl px-12 py-8 w-1/3 h-[450px] dark-rectangle" onSubmit={(e) => createRoom(e)}>
+                <div>
+                    <span className="text-3xl">Créer un salon</span>
+                    <span className=" ml-4 text-xs">Par souci de bande passante, cette fonctionnalité est restreinte.</span>
+                    <div className="flex flex-col space-y-2 my-8">
+                        <label htmlFor="createUsername">Nom d&apos;utilisateur</label>
+                        <input required id="createUsername" type="text" value={username} className="bg-gray-700 rounded-lg px-4 py-2 outline outline-white/10" onChange={(e) => setUsername(e.target.value)}></input>
+                    </div>
+                    <div className="flex flex-col space-y-2 mb-4">
+                        <label htmlFor="token">Jeton d'authentification</label>
+                        <input required id="token" type="password" value={token} className="bg-gray-700 rounded-lg px-4 py-2 outline outline-white/10" onChange={(e) => setToken(e.target.value)}></input>
+                    </div>
+                    {createError && (
+                        <span className="text-red-500">{createError}</span>
+                    )}
+                </div>
+                <button disabled={loading} className="py-4 mt-8 w-full cursor-pointer dark-button disabled:cursor-not-allowed">{loading ? "Création en cours... Veuillez patienter." : "Créer un salon"}</button>
+            </form>
         </div>
     )
 }
