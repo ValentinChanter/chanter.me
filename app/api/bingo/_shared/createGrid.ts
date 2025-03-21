be0ca17400df31904e1e5662ae13a8d00a18188e
ValentinChanter <valentinsing19@gmail.com>
2025-03-21 17:06:22 +0100

    Add file read by line instead of loading the whole file to greatly reduce memory usage

--- app/api/bingo/_shared/createGrid.ts
+++ app/api/bingo/_shared/createGrid.ts
@@ -1,11 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
+import * as readline from "readline";
 
 import { CONFIG } from "@/config/bingo";
+import { r2 } from '@/lib/r2';
 
 import { GetObjectCommand } from '@aws-sdk/client-s3'
-
-import { r2 } from '@/lib/r2';
 
 function initGrid(words: string[]) {
     const grid = [] as { word: string, colors: string[] }[];
@@ -16,12 +16,50 @@ function initGrid(words: string[]) {
     return grid;
 }
 
-export default async function createGrid() {
-    let wordlist: string[] = [];
-    
-    const data = fs.readFileSync(path.join(process.cwd(), "public",  `frwiki-${CONFIG.date}-all-titles-in-ns-0`), "utf8");
+// We'll get 2n random words from the wordlist, and statiscally, we should get at least n words that are not in the blacklist (and by far)
+async function getNWords(stream: fs.ReadStream | NodeJS.ReadableStream, n: number ) {
+    const lines = new Set<number>(); // Set to avoid duplicates
 
-    if (!data) {
+    // Get 2n random lines
+    while (lines.size < 2 * n) {
+        const line = Math.floor(Math.random() * CONFIG.linecount);
+
+        lines.add(line);
+    }
+
+    const maxLine = Math.max(...Array.from(lines));
+
+    const rl = readline.createInterface({
+        input: stream,
+        crlfDelay: Infinity,
+    });
+
+    const words = [] as { word: string, line: number }[];
+    let i = 1;
+    for await (const line of rl) {
+        if (lines.has(i) && !CONFIG.wordBlacklist.some(regex => regex.test(line))) {
+            words.push({ word: line, line: i });
+        }
+
+        // Stop early if we reach the max line or if we have enough words
+        if (i === maxLine || words.length === n) break;
+        i++;
+    }
+
+    // Put the words in the order the lines were picked at the start (shuffling, in a way)
+    // We could also do regular shuffling but would be a waste not to reuse the lines we picked
+    return words.sort((a) => lines.has(a.line) ? -1 : 1).map(({ word }) => word);
+}
+
+export default async function createGrid() {
+    let stream;
+
+    const filePath = path.join(process.cwd(), "public", `frwiki-${CONFIG.date}-all-titles-in-ns-0`);
+    if (fs.existsSync(filePath)) {
+        stream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+    } else {
+        console.log("[WARN] Reading wordlist from S3 Bucket");
+
         const file = await r2.send(new GetObjectCommand({
             Bucket: process.env.R2_BUCKET_NAME,
             Key: `frwiki-${CONFIG.date}-all-titles-in-ns-0`,
@@ -31,34 +69,14 @@ export default async function createGrid() {
             return new Response("Wordlist not found", { status: 500 });
         }
 
-        const data = await file.Body.transformToString();
-        wordlist = data.split("\n").filter(word => !CONFIG.wordBlacklist.some((regex) => regex.test(word)));
-
-        if (wordlist.length === 0) {
-            return new Response("Wordlist empty", { status: 500 });
-        }
-
-        console.log("[WARN] Reading wordlist from R2 Bucket");
+        stream = file.Body as NodeJS.ReadableStream;
     }
 
-    wordlist = data.split("\n").filter(word => !CONFIG.wordBlacklist.some((regex) => regex.test(word)));
 
-    if (wordlist.length === 0) {
-        return new Response("Wordlist empty", { status: 500 });
-    }
+    const words = await getNWords(stream, 25 + 1);
+    const startWord = words.pop();
 
-    const words = [] as string[];
+    if (!startWord) return new Response("Start word not found", { status: 500 });
 
-    if (wordlist.length <= 25) {
-        words.push(...wordlist);
-    } else {
-        while (words.length < 25) {
-            const word = wordlist[Math.floor(Math.random() * wordlist.length)];
-            if (!words.includes(word)) {
-                words.push(word);
-            }
-        }
-    }
-
-    return initGrid(words);
+    return { grid: initGrid(words), startWord };
 }
