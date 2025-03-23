248796f11eb3ac9f48d136c73cb677bbcee119b8
ValentinChanter <valentinsing19@gmail.com>
2025-03-23 14:34:45 +0100

    Add room owner name and player count in room description

--- app/bingo/[code]/layout.tsx
+++ app/bingo/[code]/layout.tsx
@@ -1,7 +1,37 @@
-export async function generateMetadata({ params }) {
+type Props = {
+  params: Promise<{ code: string }>
+};
+
+export async function generateMetadata({ params }: Props) {
+  const { code } = await params;
+
+  const baseUrl = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:3000";
+
+  const res = await fetch(`${baseUrl}/api/bingo/getRoomOwnerAndPlayerCount`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ code })
+  })
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  });
+
+  if (!res) {
+    return {
+      title: "Wikirace Bingo | chanter.me",
+      description: "Créez ou rejoigner un salon pour jouer au Wikirace Bingo contre vos amis !"
+    };
+  }
+
+  const { owner, playerCount } = res as { owner: string, playerCount: number };
+
   return {
-    title: `Wikirace Bingo - Salon ${params.code} | chanter.me`,
-    description: "Rejoignez ce salon de Wikirace Bingo pour affronter vos amis !"
+    title: `Wikirace Bingo - Salon ${code} | chanter.me`,
+    description: `Rejoignez ce salon maintenant pour affronter ${owner}${playerCount === 1 ? "" : ` et ${playerCount} autres personnes`} !`
   };
 }
 
