6951000fc0fde7976ccbc335b50a349577297ee4
ValentinChanter <valentinsing19@gmail.com>
2025-03-23 14:43:00 +0100

    Fix typo

--- app/bingo/layout.tsx
+++ app/bingo/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Wikirace Bingo | chanter.me",
-  description: "Créez ou rejoigner un salon pour jouer au Wikirace Bingo contre vos amis !",
+  description: "Créez ou rejoignez un salon pour jouer au Wikirace Bingo contre vos amis !",
 };
 
 export default function BingoLayout({
