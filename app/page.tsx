08982f7f1011d6740a2ccad7a58d0970b2ad5a63
ValentinChanter <valentinsing19@gmail.com>
2025-03-23 13:55:24 +0100

    Add link to Wikirace Bingo on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div>Nothing here... for now</div>
-      </main>
+    <div className='flex h-screen'>
+      <div className="m-auto flex flex-row h-full w-full px-20 justify-center gap-16">
+        <div className="rounded-xl py-6 px-8 dark-rectangle my-auto w-[320px] flex flex-col cursor-pointer hover:scale-105 active:scale-95 transition-transform" onClick={() => router.push("/bingo")}>
+          <span className="text-3xl mb-4">Wikirace Bingo</span>
+          <span className="text-justify">Face off against your friends in a race to reach the most Wikipedia pages before your friends do!</span>
+        </div>
+        <span className="my-auto">(+ 1 unlisted page)</span>
+      </div>
     </div>
   );
 }
