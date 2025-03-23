3f0165126a5554ef8955a44d5aaa1ee8313d2362
ValentinChanter <valentinsing19@gmail.com>
2025-03-23 13:53:08 +0100

    Add more blacklisted words

--- config/bingo.ts
+++ config/bingo.ts
@@ -6,6 +6,7 @@ export const CONFIG = {
         /_\(homonymie\)$/i, // Homonymie
         /_par_ordre_alphabétique/i, // Liste par ordre alphabétique
         /^classement/i, // Classement
+        /^\(\d{1,6}\)_/i, // Les astéroides
 
         // Événements des années 1XXX/2XXX car en trop grand nombre
         /(?:1|2)\d{3}$/, // Se termine par 1XXX ou 2XXX
@@ -13,6 +14,7 @@ export const CONFIG = {
         /saison_(?:1|2)\d{3}/i, // Saison 1XXX ou 2XXX
         /(?:1|2)\d{3}_en_/, // 1XXX ou 2XXX en
         /^tournoi/i, // Tournois en général
+        /^championnat/i, // Championnats en général
         /_de_(?:1|2)\d{3}_/, // Les coups d'état et autres événements " de 1XXX/2XXX "
     ],
     colors: [
