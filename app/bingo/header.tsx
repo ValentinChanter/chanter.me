f8a738013cf20c7fa3e1c61663dc7dc3b3405cb8
ValentinChanter <valentinsing19@gmail.com>
2025-03-25 15:18:23 +0100

    Fix text scaling when not clickable yet

--- app/bingo/header.tsx
+++ app/bingo/header.tsx
@@ -5,8 +5,10 @@ export default function Header({ title, link }: { title: string, link: string })
     return (
         <div className="flex flex-row justify-between gap-8 min-h-[4rem] ring ring-white/30 items-center">
             <Image src="/logo.png" alt="Logo" width={180} height={41} />
-            <div className='hover:scale-105 active:scale-95 transition-transform'>
-                <Link className="cursor-pointer" href={link}>{title}</Link>
+            <div>
+                <Link className="cursor-pointer" href={link}>
+                    <div className='hover:scale-105 active:scale-95 transition-transform'>{title}</div>
+                </Link>
             </div>
             <div className="w-[180px]"></div>
         </div>
